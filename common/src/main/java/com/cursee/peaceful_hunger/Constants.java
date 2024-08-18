008b50008e14624e40742e8a59bceb00252ed666
jason13official <jason13official@gmail.com>
2024-08-18 17:39:36 -0400

    changes and things

--- common/src/main/java/com/cursee/peaceful_hunger/Constants.java
+++ common/src/main/java/com/cursee/peaceful_hunger/Constants.java
@@ -9,9 +9,5 @@ public class Constants {
 
 	public static final String MOD_ID = "peaceful_hunger";
 	public static final String MOD_NAME = "Peaceful Hunger";
-	public static final String MOD_VERSION = "1.0.1";
-	public static final String MC_VERSION_RAW = "[1.21.1]";
-	public static final Pair<String, String> PUBLISHER_AUTHOR = new Pair<String, String>("Lupin", "Jason13");
-	public static final Triplet<String, String, String> PRIMARY_CURSEFORGE_MODRINTH = new Triplet<String, String, String>("https://www.curseforge.com/members/lupin/projects", "https://www.curseforge.com/members/lupin/projects", "https://modrinth.com/user/Lupin/mods");
 	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
 }
