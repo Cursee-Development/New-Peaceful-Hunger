c6122794d5391530917d967b1f4a278cd6aada52
jason13official <jason13official@gmail.com>
2024-08-18 16:21:01 -0400

    remove monolib dependency

--- common/src/main/java/com/cursee/examplemod/mixin/MinecraftMixin.java
+++ common/src/main/java/com/cursee/examplemod/mixin/MinecraftMixin.java
@@ -15,9 +15,7 @@ public class MinecraftMixin {
     @Inject(at = @At("TAIL"), method = "<init>")
     private void init(CallbackInfo info) {
 
-        if (ExampleMod.debugCommon) {
-            Constants.LOG.info("This line is printed by a mixin loaded in a {} instance!", "Common");
-            Constants.LOG.info("MC Version: {}", Minecraft.getInstance().getVersionType());
-        }
+        Constants.LOG.info("This line is printed by a mixin loaded in a {} instance!", Services.PLATFORM.getPlatformName());
+        Constants.LOG.info("MC Version: {}", Minecraft.getInstance().getVersionType());
     }
 }
